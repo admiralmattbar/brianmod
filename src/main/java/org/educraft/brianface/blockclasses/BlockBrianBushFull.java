8726a6d15d5c4aa661e2f95fc118e8b0f15e52e0
admiralmattbar <mdkissel@gmail.com>
2017-05-08 21:44:24 -0700

    More work

--- src/main/java/org/educraft/brianface/blockclasses/BlockBrianBushFull.java
+++ src/main/java/org/educraft/brianface/blockclasses/BlockBrianBushFull.java
@@ -1,5 +1,9 @@
 package org.educraft.brianface.blockclasses;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.text.TextComponentString;
 import org.educraft.brianface.init.ModBlocks;
 //import org.educraft.brianface.init.ModBlocks;
 import org.educraft.brianface.init.ModItems;
@@ -12,26 +16,35 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import static sun.audio.AudioPlayer.player;
+
 public class BlockBrianBushFull extends BlockBrianBushEmpty {
 	public BlockBrianBushFull(){
 		super();
 		this.setTickRandomly(false); //Stops random growth
 	}
-	
-	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumFacing side, float hitX, float hitY, float hitZ){
+
+	@Override
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		return super.onBlockActivated(worldIn, pos, state, playerIn, hand, facing, hitX, hitY, hitZ);
+	}
+
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, ItemStack heldItem,
+									EnumFacing side, float hitX, float hitY, float hitZ){
 		
 		//This makes it work on server side.
-		if(!worldIn.isRemote)
-		{
-			
+		if(!worldIn.isRemote) {
+
 			return true;
 		}
 		else
 		{
-			EntityItem brian_jerky = new EntityItem(worldIn, playerIn.posX, playerIn.posY, playerIn.posZ, new ItemStack(ModItems.brian_jerky));
+			EntityItem brian_jerky = new EntityItem(worldIn, playerIn.posX, playerIn.posY, playerIn.posZ,
+					new ItemStack(ModItems.brian_jerky));
 			brian_jerky.setNoPickupDelay();
 			worldIn.spawnEntity(brian_jerky);
-			
+			playerIn.sendMessage(new TextComponentString("picking"));
+
 			//Now the bush has no more jerky
 			worldIn.setBlockState(pos, ModBlocks.brian_bush_empty.getDefaultState(), 2);
 			
